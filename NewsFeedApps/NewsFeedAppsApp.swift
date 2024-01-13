14447a69f836fee83c7f89be3c0cf981461deb78
Beste Kocaoğlu <bestekocaoglu@hotmail.com>
2024-01-13 16:03:31 +0300

    Initial Commit

---
+++ NewsFeedApps/NewsFeedAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsFeedAppsApp.swift
+//  NewsFeedApps
+//
+//  Created by Beste Kocaoglu on 13.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct NewsFeedAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
